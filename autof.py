270a172de4128d3ed788d0aac3af5e5533051b6a
azranajo <azranazima89@gmail.com>
2025-07-02 17:51:22 +0700

    Update

--- autof.py
+++ autof.py
@@ -1,15 +1,18 @@
-from picamera2 import Picamera2
+from picamera2 import Picamera2, Preview
 import time
 
 # Membuat objek Picamera2
 picam2 = Picamera2()
 
+# Mengatur konfigurasi resolusi (640x480)
+config = picam2.create_still_configuration(still_width=640, still_height=480)
+picam2.configure(config)
+
 # Menyalakan kamera
 picam2.start()
 
-# Mengatur resolusi gambar menjadi 640x480 (VGA)
+# Mengaktifkan autofocus (secara default sudah aktif, tetapi kamu bisa menyesuaikannya)
 picam2.set_controls({"AfMode": 3})  # Mode autofocus, 1: Single-shot
-picam2.set_controls({"Resolution": (480, 320)})  # Resolusi diubah menjadi 640x480
 
 # Menunggu autofocus untuk menyesuaikan
 time.sleep(2)  # Tunggu 2 detik agar autofocus bisa menyesuaikan
