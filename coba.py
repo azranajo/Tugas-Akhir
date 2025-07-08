a095ac1b4bb0ea874521dcc9be0607da409cf9d6
azranajo <azranazima89@gmail.com>
2025-07-09 03:29:07 +0700

    Update

--- coba.py
+++ coba.py
@@ -257,7 +257,7 @@ def recognize_number(image):
         show_preprocess_result(image, preprocessed)
         print("Ukuran input OCR:", preprocessed.shape)
 
-        text = pytesseract.image_to_string(preprocessed, config='--psm 7 -c tessedit_char_whitelist=0123456789')
+        text = pytesseract.image_to_string(preprocessed, config='--psm 8 -c tessedit_char_whitelist=0123456789')
         print("Hasil mentah OCR:", repr(text))  # Untuk debug
         return text.strip()
     except Exception as e:
