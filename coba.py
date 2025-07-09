2d0a7f8ba7cb93a384ceb654f5e73cea80c6db14
azranajo <azranazima89@gmail.com>
2025-07-10 01:14:15 +0700

    Update

--- coba.py
+++ coba.py
@@ -299,7 +299,6 @@ def preprocess_for_ocr(image):
     return binary
 
 def show_preprocess_result(original, preprocessed):
-    plt.figure(figsize=(8, 4))
     plt.subplot(1, 2, 1)
     plt.imshow(original)
     plt.title("Sebelum Preprocess")
